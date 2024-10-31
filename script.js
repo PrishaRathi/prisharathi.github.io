dafc7bb8f4d1c0981bc3f9d9eb4dd75d7ce1d231
Prisha Rathi <25509736+PrishaRathi@users.noreply.github.com>
2024-10-31 12:25:42 -0400

    Update script.js for changes in functionality

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// function hamburg(){
-//     const navbar = document.querySelector(".dropdown")
-//     navbar.style.transform = "translateY(0px)"
-// }
-
-// function cancel(){
-//     const navbar = document.querySelector(".dropdown")
-//     navbar.style.transform = "translateY(-500px)"
-// }
-
-
-
-
-// Typewriter Effect
+// -- Typewriter Effect
 
 const texts = [
     "Developer",
